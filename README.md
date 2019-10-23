c25c995147bcccc20f89ff51646cb16b6cde7e3f
夏润豪 <610500714@qq.com>
2019-10-23 14:36:51 +0800

    ++ some changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# X-Novel
+# Taro-Novel
 一款基于Taro的小说小程序，使用了最新的Hooks方式和基于此方式的Redux，小说API接口提取自追书神器APP，仅供学习交流使用，如有侵权，请及时联系。
 
 ### 用到的技术
@@ -10,8 +10,8 @@
 
 ### 运行
 ```
-+ git clone https://github.com/Jerry-1123/X-Novel.git
-+ cd X-Novel
++ git clone https://github.com/Jerry-1123/Taro-Novel.git
++ cd Taro-Novel
 + npm install
 + npm run dev:weapp
 + 使用测试AppId，勾选"不校验和法域名"
--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -8,13 +8,13 @@ function About() {
     return (
         <View className='about'>
             <View className='content'>
-                <View className='title'>关于X-Novel</View>
+                <View className='title'>关于Taro-Novel</View>
                 <View className='tip'>介绍</View>
                 <View className='sku'>· 基于Taro的小说小程序，使用了最新的Hooks方式和基于此方式的Redux，小说API接口提取自追书神器APP，仅供学习交流使用，如有侵权，请及时联系。</View>
                 <View className='line'></View>
                 <View className='tip'>运行</View>
-                <View className='sku'>· git clone https://github.com/Jerry-1123/X-Novel.git</View>
-                <View className='sku'>· cd X-Novel</View>
+                <View className='sku'>· git clone https://github.com/Jerry-1123/Taro-Novel.git</View>
+                <View className='sku'>· cd Taro-Novel</View>
                 <View className='sku'>· npm install</View>
                 <View className='sku'>· npm run dev:weapp</View>
                 <View className='sku'>· demo小程序建议勾选"不校验合法域名"选项</View>
